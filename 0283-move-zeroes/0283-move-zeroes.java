03ebac27bb863d119e7ef8224935b80a30e3ff0c
Bushra Khan <91953463+bushra808@users.noreply.github.com>
2023-07-13 16:29:31 +0530

    Time: 1 ms (100%), Space: 45.3 MB (46.25%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,32 @@
+// class Solution {
+//      public void moveZeroes(int[] nums) {
+//         int snowBallSize = 0; 
+//         for (int i=0;i<nums.length;i++){
+// 	        if (nums[i]==0){
+//                 snowBallSize++; 
+//             }
+//             else if (snowBallSize > 0) {
+// 	            int t = nums[i];
+// 	            nums[i]=0;
+// 	            nums[i-snowBallSize]=t; //i change krne ki zaroorat hi ni h its so simple good lord
+//             }
+//         }
+//     }
+// }
+
 class Solution {
-     public void moveZeroes(int[] nums) {
-        int snowBallSize = 0; 
-        for (int i=0;i<nums.length;i++){
-	        if (nums[i]==0){
-                snowBallSize++; 
+    public void moveZeroes(int[] nums) {
+        
+       // method :1 
+        int count = 0;
+        
+            for(int i = 0; i<nums.length; i++){
+                if(nums[i]!= 0){
+                    nums[count++] = nums[i]; 
+                }
             }
-            else if (snowBallSize > 0) {
-	            int t = nums[i];
-	            nums[i]=0;
-	            nums[i-snowBallSize]=t; //i change krne ki zaroorat hi ni h its so simple good lord
-            }
+     while(count<nums.length) {
+           nums[count++] = 0;
         }
     }
 }
