c94c7178d9a8917676c7cae19e5392b5ef0007ab
Bushra Khan <91953463+bushra808@users.noreply.github.com>
2023-07-26 20:03:36 +0530

    Time: 199 ms (69.9%), Space: 59.4 MB (17.65%) - LeetHub

--- 2000-minimum-speed-to-arrive-on-time/2000-minimum-speed-to-arrive-on-time.java
+++ 2000-minimum-speed-to-arrive-on-time/2000-minimum-speed-to-arrive-on-time.java
@@ -3,9 +3,10 @@ import java.lang.*;
 class Solution {
     public int minSpeedOnTime(int[] dist, double hour) {
         int left = 1, right = 10000000;
+        int temp=-1;
 
-        while (left < right) {
-            int mid = left + (right - left) / 2; // to prevent overflow in case both left and right are large values
+        while (left <= right) {
+            int mid = left + (right-left) / 2; // to prevent overflow in case both left and right are large values
             double t = 0.0;
 
             for (int i = 0; i < dist.length - 1; i++) {
@@ -14,19 +15,13 @@ class Solution {
             t += dist[dist.length - 1] / (double) mid;
 
             if (t <= hour) {
-                right = mid; // we need to decrease speed to increase time (inversely proportional)
+                right = mid-1; // we need to decrease speed to increase time (inversely proportional)
+                temp = mid; // Store the correct value of mid at this point
             } else {
                 left = mid + 1;
             }
         }
 
-        // Check if the found speed is feasible and return the minimum speed
-        double t = 0.0;
-        for (int i = 0; i < dist.length - 1; i++) {
-            t += Math.ceil(dist[i] / (double) left);
-        }
-        t += dist[dist.length - 1] / (double) left;
-
-        return (t <= hour) ? left : -1;
+        return temp;
     }
 }
