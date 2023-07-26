d98dd98d944b2da00c2ed40492d7db4520597c8b
Bushra Khan <91953463+bushra808@users.noreply.github.com>
2023-07-26 20:07:38 +0530

    Time: 201 ms (68.86%), Space: 58.8 MB (81.66%) - LeetHub

--- 2000-minimum-speed-to-arrive-on-time/2000-minimum-speed-to-arrive-on-time.java
+++ 2000-minimum-speed-to-arrive-on-time/2000-minimum-speed-to-arrive-on-time.java
@@ -1,27 +1,28 @@
+import java.lang.*;
+
 class Solution {
     public int minSpeedOnTime(int[] dist, double hour) {
-        if(dist.length > Math.ceil(hour)) {
-            return -1;
-        }
+        
+        int left = 1, right = 10000000;
+        int temp=-1;
 
-        int left = 1, right = (int) 10e7 + 1;
-        while(left <= right) {
-            int mid = left + (right - left) / 2;
-            if(binarySearch(dist, mid, hour)) {
-                right = mid - 1;
+        while (left <= right) {
+            int mid = left + (right-left) / 2; // to prevent overflow in case both left and right are large values
+            double t = 0.0;
+
+            for (int i = 0; i < dist.length - 1; i++) {
+                t += Math.ceil(dist[i] / (double) mid);
+            }
+            t += dist[dist.length - 1] / (double) mid;
+
+            if (t <= hour) {
+                right = mid-1; // we need to decrease speed to increase time (inversely proportional)
+                temp = mid; // Store the correct value of mid at this point
             } else {
                 left = mid + 1;
             }
         }
-        return left;
-    }
 
-    private boolean binarySearch(int[] dist, int target, double hour) {
-        double time = 0;
-        for(double d : dist) {
-            time = Math.ceil(time);
-            time += d / target;
-        }
-        return time <= hour;
+        return temp;
     }
 }
