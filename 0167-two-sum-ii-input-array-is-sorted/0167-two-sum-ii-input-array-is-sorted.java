be5ab02c2a8e200d837d2ad431bdf722ce4c5ffd
Bushra Khan <91953463+bushra808@users.noreply.github.com>
2023-07-26 21:52:46 +0530

    Time: 1 ms (99.27%), Space: 45.6 MB (55.38%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,32 +1,20 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int n=numbers.length;
-        int pos=0;
-        int i=0;
-        for(i=0;i<n;i++)
-        {
-            pos=BinarySearch(numbers, target-numbers[i], i);
-            if(pos!=-2)
+    public int[] twoSum(int[] num, int target) {
+    int[] indice = new int[2];
+    if (num == null || num.length < 2) return indice;
+    int left = 0, right = num.length - 1;
+    while (left < right) {
+        int v = num[left] + num[right];
+        if (v == target) {
+            indice[0] = left + 1;
+            indice[1] = right + 1;
             break;
+        } else if (v > target) {
+            right --;
+        } else {
+            left ++;
         }
-        return new int[]{i+1,pos+1};
     }
-    public int BinarySearch(int[] A, int num, int j)
-    {
-        int left=0, right=A.length-1;
-        int mid=0; int ans=-2;
-        while(left<=right)
-        {
-            mid=left+(right-left)/2;
-            if(A[mid]==num && mid!=j)
-            {
-                ans=mid;
-                break; }
-            else if(A[mid]>num)
-            right=mid-1;
-            else
-            left=mid+1;
-        }
-        return ans;
-    }
+    return indice;
+}
 }
