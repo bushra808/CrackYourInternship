b5afbef22686193e2bf8c1c5b13eb5274cfec1c9
Bushra Khan <91953463+bushra808@users.noreply.github.com>
2023-08-01 02:10:14 +0530

    Time: 8 ms (98.61%), Space: 44.7 MB (20.07%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,15 +1,34 @@
 class Solution {
-        public int minEatingSpeed(int[] piles, int H) {
+    public int minEatingSpeed(int[] piles, int h) {
+        //if she eats one pile one hour, min val of k can be len cause:
+        //1) either she eats all
+        //2) or she eats<=k, in this case total time>len
+        int n=piles.length;
+        int k=n;
+
+        //do we start with k and increment it until koko ate all?
+        //....like, for binary search, let's sort the array first
+
+        
+        if(h==n)
+        {
+        Arrays.sort(piles);
+        return piles[n-1];
+        }
+         //no option left than to eat largest pile in 1 hour
+        //else koko can eat< lar in 1 hr, aaram se
+        
         int l = 1, r = 1000000000;
         while (l < r) {
             int m = (l + r) / 2, total = 0;
             for (int p : piles)
                 total += (p + m - 1) / m;
-            if (total > H)
+            if (total > h)
                 l = m + 1;
             else
                 r = m;
         }
         return l;
+
     }
 }
