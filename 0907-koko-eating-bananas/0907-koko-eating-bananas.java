437364b6e386b5b0e04e572d2e348217eef7f1e0
Bushra Khan <91953463+bushra808@users.noreply.github.com>
2023-08-01 02:11:05 +0530

    Time: 7 ms (98.73%), Space: 44.4 MB (43.97%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -18,16 +18,19 @@ class Solution {
          //no option left than to eat largest pile in 1 hour
         //else koko can eat< lar in 1 hr, aaram se
         
-        int l = 1, r = 1000000000;
-        while (l < r) {
-            int m = (l + r) / 2, total = 0;
+        int l=1; int r=1000000000;
+        while(l<r)
+        {
+            int m=(l+r)/2;
+            int total=0;
             for (int p : piles)
-                total += (p + m - 1) / m;
-            if (total > h)
-                l = m + 1;
+            total += (p+m-1)/m;
+            if(total>h)
+            l=m+1; //l inc, m inc, total dec
             else
-                r = m;
+            r=m;
         }
+
         return l;
 
     }
