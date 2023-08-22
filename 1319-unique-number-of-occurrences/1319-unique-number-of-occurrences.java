62beb614e857b89b2dbb45a788c7c0cc3298c636
Bushra Khan <91953463+bushra808@users.noreply.github.com>
2023-08-22 22:02:05 +0530

    Time: 2 ms (95.44%), Space: 40.3 MB (92.19%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,10 +1,26 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Count occurrences of each value in the array
         for (int num : arr) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        Set<Integer> set = new HashSet<>(map.values());
-        return map.size() == set.size();
+
+        // Use a HashSet to keep track of unique occurrence values
+        HashSet<Integer> uniqueOccurrences = new HashSet<>();
+
+        // Iterate through the occurrences in the hashmap
+        for (int occurrences : map.values()) {
+            // If the same occurrence count is already in the set, return false
+            if (uniqueOccurrences.contains(occurrences)) {
+                return false;
+            }
+            // Otherwise, add the occurrence count to the set
+            uniqueOccurrences.add(occurrences);
+        }
+
+        // If we've reached here, all occurrence counts are unique
+        return true;
     }
 }
