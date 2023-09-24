25d2f3fad3b6783b4aeef679061cee0799f5bb40
Bushra Khan <91953463+bushra808@users.noreply.github.com>
2023-09-24 20:11:21 +0530

    Time: 1 ms (99.86%), Space: 48.5 MB (46.05%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,29 +1,65 @@
-class Solution {
-    public int majorityElement(int[] nums) {
+// class Solution {
+//     public int majorityElement(int[] nums) {
         
+//         int n=nums.length;
+//         HashMap<Integer, Integer> map=new HashMap<>();
+//         for(int i=0; i<n; i++)
+//         {
+//             if(!map.containsKey(nums[i]))
+//             {
+//                 map.put(nums[i], 1);
+//             }
+//             else
+//             {
+//                 int count=map.get(nums[i]);
+//                 map.put(nums[i], count + 1);
+//             }
+//         }
+
+//         for(int key: map.keySet())
+//         {
+//             int count=map.get(key);
+//             if(count>n/2){
+//             return key;
+//             }
+//         }
+
+//         return -1;
+//     }
+// }
+
+//MOORE'S VOTING ALGORITHM
+
+class Solution {
+    public int majorityElement(int[] nums)
+    {
         int n=nums.length;
-        HashMap<Integer, Integer> map=new HashMap<>();
+        int can=nums[0];
+        int count=1;
+        for(int i=1; i<n; i++)
+        {
+            if(can==nums[i])
+            count++;
+            else
+            count--;
+
+            if(count==0)
+            {
+                can=nums[i];
+                count=1;
+            }
+        }
+        
+        count=0;
         for(int i=0; i<n; i++)
         {
-            if(!map.containsKey(nums[i]))
-            {
-                map.put(nums[i], 1);
-            }
-            else
-            {
-                int count=map.get(nums[i]);
-                map.put(nums[i], count + 1);
-            }
+            if(can==nums[i])
+            count++;
         }
 
-        for(int key: map.keySet())
-        {
-            int count=map.get(key);
-            if(count>n/2){
-            return key;
-            }
-        }
-
+        if(count>n/2)
+        return can;
+        else
         return -1;
     }
 }
