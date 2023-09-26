d663e2eb4473971e9290f5733e10932b85d5e27f
Bushra Khan <91953463+bushra808@users.noreply.github.com>
2023-09-27 00:56:45 +0530

    Time: 1 ms (71.54%), Space: 41.3 MB (95.77%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,24 +1,43 @@
+// class Solution {
+//     public List<List<Integer>> subsets(int[] nums) {
+//         ArrayList<List<Integer>> sets = new ArrayList<>();
+//         findSets(nums, 0, new ArrayList<>(), sets);
+//         return sets;
+//     }
+
+//     public void findSets(int[] nums, int i, ArrayList<Integer> currentSet, ArrayList<List<Integer>> sets) {
+//         if (i == nums.length) {
+//             sets.add(new ArrayList<>(currentSet)); // Add a copy of the currentSet to sets
+//             return;
+//         }
+
+//         // Exclude the current element
+//         findSets(nums, i + 1, currentSet, sets);
+
+//         // Include the current element
+//         currentSet.add(nums[i]);
+//         findSets(nums, i + 1, currentSet, sets);
+
+//         // Backtrack (remove the current element) to explore other subsets
+//         currentSet.remove(currentSet.size() - 1);
+//     }
+// }
+
+
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        ArrayList<List<Integer>> sets = new ArrayList<>();
-        findSets(nums, 0, new ArrayList<>(), sets);
+        List<List<Integer>> sets = new ArrayList<>();
+        generateSubsets(nums, 0, new ArrayList<>(), sets);
         return sets;
     }
 
-    public void findSets(int[] nums, int i, ArrayList<Integer> currentSet, ArrayList<List<Integer>> sets) {
-        if (i == nums.length) {
-            sets.add(new ArrayList<>(currentSet)); // Add a copy of the currentSet to sets
-            return;
+    private void generateSubsets(int[] nums, int start, List<Integer> current, List<List<Integer>> sets) {
+        sets.add(new ArrayList<>(current));
+
+        for (int i = start; i < nums.length; i++) {
+            current.add(nums[i]);
+            generateSubsets(nums, i + 1, current, sets);
+            current.remove(current.size() - 1);
         }
-
-        // Exclude the current element
-        findSets(nums, i + 1, currentSet, sets);
-
-        // Include the current element
-        currentSet.add(nums[i]);
-        findSets(nums, i + 1, currentSet, sets);
-
-        // Backtrack (remove the current element) to explore other subsets
-        currentSet.remove(currentSet.size() - 1);
     }
 }
