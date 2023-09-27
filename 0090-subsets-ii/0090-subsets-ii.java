1a28cc26cf06eb83014ee797482f490f382bfb1b
Bushra Khan <91953463+bushra808@users.noreply.github.com>
2023-09-27 16:12:47 +0530

    Time: 1 ms (99.93%), Space: 43.6 MB (58.12%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,29 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums); // Sort the input array to group duplicates together
-        HashSet<List<Integer>> sets = new HashSet<>();
-        findSets(nums, 0, new ArrayList<>(), sets);
-        List<List<Integer>> result = new ArrayList<>(sets);
-        return result;
+        Arrays.sort(nums);
+        List<List<Integer>> ansList = new ArrayList<>();
+        findSubsets(0, nums, new ArrayList<>(), ansList);
+        return ansList;
     }
 
-    public void findSets(int[] nums, int i, ArrayList<Integer> currSet, HashSet<List<Integer>> sets) {
-        if (i == nums.length) {
-            sets.add(new ArrayList<>(currSet));
-            return;
+    private void findSubsets(int ind, int[] nums, List<Integer> ds, List<List<Integer>> ansList) {
+        ansList.add(new ArrayList<>(ds));
+        for (int i = ind; i < nums.length; i++) {
+            if (i != ind && nums[i] == nums[i - 1]) continue;
+            ds.add(nums[i]);
+            findSubsets(i + 1, nums, ds, ansList);
+            ds.remove(ds.size() - 1);
         }
-
-        // Include the current element
-        currSet.add(nums[i]);
-        findSets(nums, i + 1, currSet, sets);
-
-        // Exclude the current element and any duplicates
-        while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
-            i++;
-        }
-        currSet.remove(currSet.size() - 1);
-
-        // Continue generating subsets without the duplicate element
-        findSets(nums, i + 1, currSet, sets);
     }
+
+   
+    
 }
