8a79c272dedc85a92cf2fe5e6c0f9737da056241
Bushra Khan <91953463+bushra808@users.noreply.github.com>
2023-10-01 11:40:34 +0530

    Time: 2 ms (37.35%), Space: 43.6 MB (87.16%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,33 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>(); //for answer
-        List<Integer> ds=new ArrayList<>(); // a data struture to store individual permutations
-        boolean freq[]=new boolean[nums.length]; //freq to mark elements
-        recurpermute(nums, ds, ans, freq);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        backtrack(nums, current, ans);
         return ans;
-        
     }
 
-    public void recurpermute(int[] nums, List<Integer> ds, List<List<Integer>> ans, boolean[] freq)
-    {
-        if(ds.size()==nums.length) //base case
-        {
-            ans.add(new ArrayList<>(ds)); //add data str to ans
+    private void backtrack(int[] nums, List<Integer> current, List<List<Integer>> ans) {
+        if (current.size() == nums.length) {
+            ans.add(new ArrayList<>(current)); // Add a valid permutation to the result
             return;
         }
 
-        for(int i=0;i<nums.length;i++)
-        {
-            if(!freq[i]) //not marked
-            {
-                freq[i]=true; //mark it
-                ds.add(nums[i]); //add unmarked ele to ds
-                recurpermute(nums, ds, ans, freq); //call recursive
-                ds.remove(ds.size()-1); //remove the element we added
-                freq[i]=false; //unmark it
+        for (int i = 0; i < nums.length; i++) {
+            if (!current.contains(nums[i])) { // Check if the element is not already in the current permutation
+                current.add(nums[i]); // Add the element to the current permutation
+                backtrack(nums, current, ans); // Recursively generate the next part of the permutation
+                current.remove(current.size() - 1); // Backtrack by removing the last added element
             }
         }
     }
-
-
 }
